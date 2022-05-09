83abe9ff834cd85aab77f4dfcb80f759c07bfc06
rajeshsaharan <16072000rajesh@gmail.com>
2022-05-09 12:06:03 +0530

    javascript

---
+++ js/begginer.js
@@ -0,0 +1,9 @@
+// to show output we can use console.log()
+console.log("hello world") ;// hello world //double quotes
+console.log('hello  world'); // hello world // single quotes
+
+//; semicolan is used on every ending of sentance
+
+console.log(`hello world`) //hellow world using backtics
+
+//to comment use "//" any line
---
+++ js/variables.js
@@ -0,0 +1,17 @@
+// variables can store information
+
+//declare a variable
+
+var Fname = "rajesh"  //string stored Fname variable string are stored in 
+//single or double quotes
+//var is a javascript keyword
+
+//variables can use later in our program
+
+console.log(Fname) // variable used 
+//variables are case sensetive 
+
+//variable can change (new value assigned)
+
+Fname = "amit" //reassigned
+console.log(Fname)

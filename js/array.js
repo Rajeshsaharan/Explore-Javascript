355b660eca595f0365a32adbb294df01703a43cf
rajeshsaharan <16072000rajesh@gmail.com>
2022-05-21 17:25:41 +0530

    javascript

---
+++ js/array.js
@@ -0,0 +1,31 @@
+//intro to arrays
+//refrence data types----object
+
+//array :- ordered collections of items/elements
+//can store any data types 
+
+let fruits = ['mango', 'banana', 'apple']
+
+//array have also index
+console.log(fruits) //shows full array
+
+console.log(fruits[0]) //output -- mango
+
+let mixedArray = ['1', 1, 'apple', null] //any data types
+
+//array is mutable
+
+fruits[0] = "grapes"
+
+console.log(fruits) // main array is changed
+
+console.log(typeof fruits) //object --- refrence data type
+
+//check array
+
+
+console.log(Array.isArray(fruits)) /// true
+
+
+
+

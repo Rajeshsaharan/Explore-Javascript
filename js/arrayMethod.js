68ba6f45b0f9defcb2668e78a49e1f39868d49df
rajeshsaharan <16072000rajesh@gmail.com>
2022-05-21 18:15:42 +0530

    javascript

---
+++ js/arrayMethod.js
@@ -0,0 +1,20 @@
+//array methods
+
+//array push ---> add items in the ending
+
+let fruits = ["apple", "banana"]
+
+fruits.push("mango") ///....pushed mango in as last element
+
+//pop
+fruits.pop() //remove last element from original array and return last item
+
+//unshift --- add item to zero index
+
+fruits.unshift("starwberry", "blackberry")  //add items to zero
+
+//shift -- remove items from zero
+
+fruits.shift() //remove item from zero index & return first item
+
+

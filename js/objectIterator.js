bef502bd0fbf25f9311d3f6867202868f57caee1
rajeshsaharan <16072000rajesh@gmail.com>
2022-05-22 08:27:09 +0530

    javascript

---
+++ js/objectIterator.js
@@ -0,0 +1,23 @@
+//how to iterate in object
+
+
+const person = {
+    name:"rajesh",
+    age: 23,
+    lang: [ 'python', 'javascript', 'solidity'] 
+}
+
+//for in loop---> provide key from an object
+for (let key in person){
+    console.log(`${key}:${person[key]}`)
+}
+
+//another method
+
+Object.keys(person) // will gives array of all key
+
+//for of loop
+for (let key of Object.keys(person)){
+    console.log(person[keys])
+}
+

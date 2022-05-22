8613be5e6e8c1fac799924ff2ce5c5770a0003b9
rajeshsaharan <16072000rajesh@gmail.com>
2022-05-22 07:11:11 +0530

    javascript

---
+++ js/forInLoopArray.js
@@ -0,0 +1,6 @@
+//for in loop --- provide index of an array
+const fruits = ["apple", "banana", "grapes"]
+
+for (let index in fruits){
+    console.log(fruits[index])
+}
---
+++ js/forOfLoopArray.js
@@ -0,0 +1,10 @@
+//for of loop for arrray
+//--- proivde all items of an array
+
+const fruits = ["apple", "mango", "bananna"]
+
+for (let fruit of fruits){
+    console.log(fruit)
+}
+
+//for of loop runs until the array length

2ef563130ca2a8a0ebbf59310e13a51367069c30
rajeshsaharan <16072000rajesh@gmail.com>
2022-05-22 06:44:26 +0530

    javascript

---
+++ js/forLoopArray.js
@@ -0,0 +1,28 @@
+//for loop for array
+
+let fruits = ['mango', 'banana', 'grapes']
+
+//array length property
+
+// array1.length ---- gives you length of an array
+
+for (let i = 0 ; i< fruits.length; i++){
+    console.log(fruits[i])
+
+}
+
+//while loop
+
+let i = 0
+while(i < fruits.length){
+    console.log(fruits[i])
+    i++;
+}
+
+//do while loop
+let x = 0;
+do{
+    console.log(fruits[x])
+    i++
+}while(x<fruits.length)
+

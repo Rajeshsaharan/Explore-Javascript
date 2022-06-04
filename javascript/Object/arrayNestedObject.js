b23b3d81a23dc372cd16b545de8bc858ea25f04c
rajeshsaharan <16072000rajesh@gmail.com>
2022-06-04 10:44:47 +0530

    javascript

---
+++ javascript/Object/arrayNestedObject.js
@@ -0,0 +1,40 @@
+//object inside array
+//very useful in real application
+
+const users = [
+    {
+        id : 1,
+        username : "xbyo",
+        age: 23
+    },
+    {
+        id : 2,
+        username : "bubu",
+        age: 21
+    }
+]
+
+//for loop
+
+for (let user of users){
+    console.log(user) // iterate through array
+}
+
+//nested destructuring
+
+const [user1, user2] = users
+console.log(user1) // return first object from array
+console.log(user2) //retrun second object from array
+
+
+const [{id}, {username}] = users // object & array both destrcuture with same key name variable
+
+console.log(id) // first object id
+console.log(username) // second object username
+
+//custom name variable
+const [{id:userId}, {username:UserName_of_user}] = users
+
+console.log(userId) //fetch id from first object (custom variables)
+console.log(UserName_of_user) //fetch username from second object (custom Variables)
+
--- javascript/Object/destructrurObject.js
+++ javascript/Object/destructrurObject.js
@@ -1,7 +1,8 @@
 const profile = {
     'name1' : "rajeshsaharan123",
     'bio': "hey way to god",
-    'id': 1
+    'id': 1,
+    'course': "brt"
 }
 
 //old ways to declare variable from object
@@ -10,17 +11,19 @@ let user_bio = profile.bio //or profile["bio"]
 
 //new ways to declare variable from object ---> known as object destructruing
 
-let {name1 , bio } = profile //create variable with same key name 
+let {name1 , bio , ...restprops} = profile //create variable with same key name 
 
 console.log(name1) // rajeshsahran123
 console.log(bio) // hey way to god
+console.log(restprops) // {"id": 1, "course": "brt"}
 
 
 //create variable apart from key name form destructring
 
-let {name1:username1, bio:user_bio1} = profile
+let {name1:username1, bio:user_bio1, ...restprops1} = profile
 
 
 console.log(username1) // rajeshsaharan123
 console.log(user_bio1) // hey way to handle
+console.log(restprops1) // represent a object with rest key or value
 

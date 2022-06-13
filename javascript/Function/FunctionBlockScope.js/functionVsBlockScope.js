9030754e57a109bc260142b37a3ef999ef2608dc
rajeshsaharan <16072000rajesh@gmail.com>
2022-06-13 18:30:23 +0530

    javascript

---
+++ javascript/Function/FunctionBlockScope.js/functionVsBlockScope.js
@@ -0,0 +1,23 @@
+.//first read file variables/variablesKeywords.js ----> IMPORTANT
+//block scope or function scope
+
+//let or const are block scope
+//var is function scope
+
+// { } ----> block
+//& whole document or .js file is also a block
+
+{
+    let firstName = "rajesh"
+    var lastName = "saharan"
+    const age = 23
+    console.log(firstName) //access from block & reassigned but redeclared in same 
+    //block scope
+    console.log(age) //access from block & cant reassigned & redeclared in same block scope
+}
+
+
+console.log(lastName) // var can be access fro outside block & reassigned & redeclared
+
+
+

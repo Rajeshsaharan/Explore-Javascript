2340a5eb53f306d0c81ec14a94bd24a802f1a0cd
rajeshsaharan <16072000rajesh@gmail.com>
2022-06-16 19:43:39 +0530

    javascript

--- javascript/Array/Important array Method/fillMethod.js
+++ javascript/Array/Important array Method/fillMethod.js
@@ -3,3 +3,8 @@
 
 const myarray = new Array(10).fill(-1) // [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
 
+//fill -- to delete or insert something on middle of any array
+//splice -- to delete or add something on middle of any array
+//change original array
+
+//read more about it on google.com

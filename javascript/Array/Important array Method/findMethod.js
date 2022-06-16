8e7470bf360485e2f8a13f5f14a6d31bd32f99e1
rajeshsaharan <16072000rajesh@gmail.com>
2022-06-16 19:10:55 +0530

    javascript

--- javascript/Array/Important array Method/findMethod.js
+++ javascript/Array/Important array Method/findMethod.js
@@ -7,3 +7,16 @@ const ans = anyArray.find((string)=> string.length ===3 )
 
 console.log(ans) //output ----> ram
 
+//real world example
+
+
+const persons = [
+    {usrId : 1, firstName : 'rajesh'},
+    {usrId :2, firstName : "amit"},
+    {usrId :3, firstName: "ankit"}
+]
+
+
+const my3User = persons.find((user)=>user.usrId=== 3)
+
+console.log(my3User) // output -----> {userId: 3, firstName : "ankit"}

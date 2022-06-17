4a505bb8fe09bf10782b8a893c0653bf03adca31
rajeshsaharan <16072000rajesh@gmail.com>
2022-06-17 07:30:15 +0530

    javascript

--- javascript/Methods/callApplyBind.js
+++ javascript/Methods/callApplyBind.js
@@ -30,3 +30,17 @@ function outsideFunc(course, id){
 
 outsideFunc.call(user1, "brt", 123) // this === user1 
 outsideFunc.call (user2, "bpt", 125) //output "amit", "male", "bpt", 125 //this === user2
+
+
+//apply methods
+
+outsideFunc.apply(user1, ["brt", 123]) //call & apply both are same but apply pass the argu as array form
+
+
+//bind method
+
+//bind return a function
+
+const newfunc = outsideFunc.bind(user1, 'brt', 123)
+
+newfunc() //can be called further

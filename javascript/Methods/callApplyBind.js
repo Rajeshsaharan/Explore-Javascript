703bee67022856723bc5a1cd4b4947f3ad23f4e2
rajeshsaharan <16072000rajesh@gmail.com>
2022-06-17 07:23:17 +0530

    javascript

---
+++ javascript/Methods/callApplyBind.js
@@ -0,0 +1,32 @@
+// call apply & bind methods
+
+const user1 = {
+    firstName : "rajesh",
+    gender : "male",
+    about : function(){
+        console.log(this.firstName, this.gender)
+    }
+}
+
+user1.about() //output ---> rajesh, male
+user1.about.call(user1) // same output
+
+const user2  = {
+    firstName : "amit",
+    gender : "male"
+}
+
+
+
+
+
+user1.about.call(user2) //this refer to user2 object
+
+
+//we can define function outside & can be add to every object using call methof
+function outsideFunc(course, id){
+    console.log(this.firstName, this.gender, course, id)
+}
+
+outsideFunc.call(user1, "brt", 123) // this === user1 
+outsideFunc.call (user2, "bpt", 125) //output "amit", "male", "bpt", 125 //this === user2

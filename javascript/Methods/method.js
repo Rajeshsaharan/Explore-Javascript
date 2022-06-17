2b524d4fad758b03db77d6efdc420dcb3d3f38a9
rajeshsaharan <16072000rajesh@gmail.com>
2022-06-17 07:10:46 +0530

    javascript

---
+++ javascript/Methods/method.js
@@ -0,0 +1,38 @@
+//methods ---- function inside object
+const user = {
+    firstName : "rajesh",
+    age : 23,
+    about : function(){
+        console.log("hello world")
+    }
+}
+
+user.about() // "hello world"
+
+
+const user1 = {
+    firstName : "rajesh",
+    age : 25,
+    about : function(){
+        console.log(this.firstName, this.age)
+    }
+}
+
+user1.about() // rajesh , 23 
+
+//this refers to same object
+
+
+console.log(this) //window object
+
+
+function myfunc(){
+    console.log(this)
+}
+
+myfunc() //also refers to window object 
+
+//myfunc() added to window object as method
+
+window.myfunc() // window object
+

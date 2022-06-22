52b3cdbb03ef856587bdd934a6724fdd0be44d3d
rajeshsaharan <16072000rajesh@gmail.com>
2022-06-22 18:36:10 +0530

    javascript

--- javascript/Object Oriented javascript/newKeyword.js
+++ javascript/Object Oriented javascript/newKeyword.js
@@ -25,3 +25,22 @@ console.log(user1.__Proto__) //output -----> createUser.prototype
 //CONVENTION 
 
 //IF A FUNCTION IS CALLED WHENEVER WE USE NEW KEYWORD WE SHOULD USE FIRST LETTER CAPITAL
+
+
+// gives all keys of object + __proto__ object both keys
+
+for (let keys in user1){
+    console.log(keys)
+}
+
+//to Avoid __proto__ object keys we can use hasOwnProperty method
+
+for (let key in user1){
+    if (user1.hasOwnProperty(key)){
+        console.log(key)
+    }else{
+        console.log("this key os not from main object")
+    }
+}
+
+

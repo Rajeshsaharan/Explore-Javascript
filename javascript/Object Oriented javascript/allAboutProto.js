436cf7c261d596d1775c9d893ccae4aa666a0955
rajeshsaharan <16072000rajesh@gmail.com>
2022-06-22 18:45:26 +0530

    javascript

---
+++ javascript/Object Oriented javascript/allAboutProto.js
@@ -0,0 +1,12 @@
+// const list = [1, 2,3]
+// actually it works internally that way
+
+const list = new Array(1,2,3)
+
+//array is a constructor function 
+//array.prototype also have many methods that are setted to list.__proto__
+
+// Array constructor set Object.create(Array.prototype) to list.__proto__
+console.log(list.__proto__)
+
+console.log(Array.prototype)
